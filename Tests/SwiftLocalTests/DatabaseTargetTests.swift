5951516b21bbe4f0848b9988edba95f515a09a5d
Andre Pham <andrepham33@gmail.com>
2025-03-30 18:49:54 +1100

    pipeline debugging 4

--- Tests/SwiftLocalTests/DatabaseTargetTests.swift
+++ Tests/SwiftLocalTests/DatabaseTargetTests.swift
@@ -31,15 +31,11 @@ final class DatabaseTargetTests: XCTestCase {
         try await self.localDatabase.clearDatabase()
     }
 
-    func testWrite() async {
-        do {
-            let record = Record(data: self.student1)
-            try await self.localDatabase.write(record)
-            let count = try await self.localDatabase.count()
-            XCTAssertEqual(count, 1)
-        } catch {
-            XCTFail("Unexpected error: \(error)")
-        }
+    func testWrite() async throws {
+        let record = Record(data: self.student1)
+        try await self.localDatabase.write(record)
+        let count = try await self.localDatabase.count()
+        XCTAssertEqual(count, 1)
     }
     
     func testReadByObjectType() async throws {
